868db2b08a1476bc430cb6b8a08db9786f312dc9
Jonas Rutishauser <jonas.rutishauser@alumni.ethz.ch>
2023-08-20 18:08:51 +0200

    Ensure that test properties are set as early as possible

--- cdi-test-mp-config/src/main/java/com/github/jonasrutishauser/cdi/test/microprofile/config/TestProperties.java
+++ cdi-test-mp-config/src/main/java/com/github/jonasrutishauser/cdi/test/microprofile/config/TestProperties.java
@@ -1,5 +1,7 @@
 package com.github.jonasrutishauser.cdi.test.microprofile.config;
 
+import static jakarta.interceptor.Interceptor.Priority.LIBRARY_BEFORE;
+
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -7,6 +9,8 @@ import java.util.Map;
 import com.github.jonasrutishauser.cdi.test.api.TestInfo;
 import com.github.jonasrutishauser.cdi.test.api.context.TestScoped;
 
+import jakarta.annotation.Priority;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.context.Initialized;
 import jakarta.enterprise.event.Observes;
 
@@ -24,7 +28,11 @@ class TestProperties {
         return initialized;
     }
 
-    void setTestProperties(@Observes @Initialized(TestScoped.class) TestInfo testInfo) {
+    void setTestProperties(@Observes @Priority(LIBRARY_BEFORE) @Initialized(ApplicationScoped.class) Object event, TestInfo testInfo) {
+        setTestProperties(testInfo);
+    }
+
+    void setTestProperties(@Observes @Priority(LIBRARY_BEFORE) @Initialized(TestScoped.class) TestInfo testInfo) {
         for (ConfigPropertyValue property : testInfo.getTestClass().getAnnotationsByType(ConfigPropertyValue.class)) {
             properties.put(property.name(), property.value());
         }
